cbae08e252fc02c95d45ac582230a764aa346f9b
jamellknows <jamellsamuels@googlemail.com>
2023-01-31 01:59:28 +0000

    forecst css

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 const API_KEY = "cc67b19dc916f461ab3e7d8d3c68bb27"
 
-
-
 let previousSearches = JSON.parse(localStorage.getItem('previousSearches'))
-$('#results-list').hide()
-if(previousSearches == null)
-{
-    console.log("new storage")
-    previousSearches = []
-}
 
-if(previousSearches.length > 5){
-  previousSearches = previousSearches.splice(previousSearches.length-5)
-}
-let previousBoolean = false
-let city_name;
-var clear;
+$(document).ready(function(){
+
+    previousDisplay()
+    $('#results-list').hide()
+    if(previousSearches == null)
+    {   
+    console.log('new storage')
+    previousSearches = []
+    }
+    previousSearches = checkPreviousLength(previousSearches)
+
+ 
+    let previousBoolean = false
+    let city_name;
+    var clear;
+    })
+
     
    let search =  function(city_name, previousBoolean=false){
     event.preventDefault()
@@ -28,8 +31,11 @@ var clear;
     Moment.format('Do MMMM')
     let date = String(Moment._d).slice(0,15)
     let previousElem = []
+    
+    console.log(previousBoolean + " " + city_name)
     if(previousBoolean == false){
         city_name = $('#search-input').val().trim()
+        previousElem.push(city_name)
     }
     let cityQuery =`http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&limit=5&appid=${API_KEY}`
     cityQuery = String(cityQuery)
@@ -57,7 +63,6 @@ var clear;
                let forecastWind = String(data.list[i].wind.speed)
                let forecastTime = date
                let icon = data.list[i].weather[0].icon
-               console.log(icon) 
                 let forecastTemplate = `<div class="forecastEl">
                                         <p class="result">` + forecastTime + ` </p>
                                         <img style="width:'2px;height: '2px';" alt ="weather" src="http://openweathermap.org/img/wn/${icon}@2x.png">
@@ -79,16 +84,23 @@ var clear;
                 
             }
             let forecastCityName = String(data.city.name)
-            let previousForecast = [forecastCityName, data]
-            previousElem.push(previousForecast)
+            if(!previousBoolean){
+                let previousForecast = [forecastCityName, data]
+                previousElem.push(previousForecast)
+            }
+            
             
 
             
+        },
+        error: function(previousSearches){
+            console.log('error previous seraches inviked')
         }
        }).then(function(response){
         let city = response.city
         let data = response.list
         $('#city').text(`${date} : ${city.name}, ${city.country}`)
+    
 
         
     
@@ -108,12 +120,21 @@ var clear;
                 $('#wind').text(`Wind Speed: ${currentWind} m/s`)
                 $('#results-list').show()
 
-                let cityName = String(data.name)
-                let previousCurrent = [cityName, data]
-                previousElem.push(previousCurrent)
-                previousElem = [previousElem]
-                previousSearches = previousSearches.concat(previousElem)
-                localStorage.setItem("previousSearches", JSON.stringify(previousSearches))
+              
+                if(previousBoolean == false){
+                    console.log('previousBoolean False')
+                    let cityName = String(data.name)
+                    let previousCurrent = [cityName, data]
+                    previousElem.push(previousCurrent)
+                    previousElem = [previousElem]
+                    previousSearches = previousSearches.concat(previousElem)
+                    previousSearches = checkPreviousLength(previousSearches)
+                    previousDisplay()
+                    console.log(previousSearches)
+                    localStorage.setItem("previousSearches", JSON.stringify(previousSearches))
+
+                }
+
                
 
 
@@ -132,14 +153,27 @@ var clear;
 
 }
 
-
+function checkPreviousLength(previousSearches)
+{
+    if(previousSearches.length > 6){
+        
+       
+        previousSearches.shift()
+      
+        
+     
+    }
+    return previousSearches
+}
 $('#search-button').click(search)
 
 $('.previousbutton').click(function(){
     clear()
-    let index = $(this).attr('data-index')
-    let searchCity = previousSearches[index][1][0]
-    search(searchCity, previous=true)
+    let index = $(this).attr('data-index')-1
+    console.log(index)
+    let searchCity = previousSearches[index][0]
+
+    search(searchCity, previousBoolean=true)
     // for the length of the data 
     
    
@@ -149,13 +183,14 @@ $('.previousbutton').click(function(){
     
 })
 function previousDisplay(){
-    if(previousSearches.length > 0){
+    if(previousSearches){
         $('.previousbutton').each(function(){
             let index = $(this).attr('data-index')-1
             let length = previousSearches.length
             if(index < length){
                 $(this).show()
-                let searchCity = previousSearches[index][1][0]
+                let searchCity = String(previousSearches[index][0])
+                console.log(searchCity)
                 $(this).text(searchCity)
             }
             
@@ -164,7 +199,7 @@ function previousDisplay(){
 
 }
 $('#search-button').click(previousDisplay)
-$(document).ready(previousDisplay)
+
 
 clear = function(){
     $('#city').empty()
